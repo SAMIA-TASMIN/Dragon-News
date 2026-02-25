230582c7424894ded7005a045554fede4ba3ca2d
unknown <novaira489@gmail.com>
2026-02-26 05:01:11 +0600

    login & register added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,13 +8,15 @@ const Register = () => {
             <h2 className="font-semibold text-2xl text-center">Register your account</h2>
           <div className="card-body">
             <fieldset className="fieldset">
+              {/* Name */}
+              <label className="label">Name</label>
+              <input type="text" className="input" placeholder="Name" />
+              <label className="label">Photo</label>
+              <input type="text" className="input" placeholder="Photo Url" />
               <label className="label">Email</label>
               <input type="email" className="input" placeholder="Email" />
               <label className="label">Password</label>
               <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
               <button className="btn btn-neutral mt-4">Register</button>
             </fieldset>
             <p>Don't Have An Account ? <Link to='/auth/login' className="text-blue-500">Login</Link> </p>
